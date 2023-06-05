b42019858e2b6f19f00697a08b532be79f62b84b
Intelligent Labs <135292690+ShizaMQ@users.noreply.github.com>
2023-06-05 13:21:43 +1000

    Update accountant.py

--- accountant.py
+++ accountant.py
@@ -12,18 +12,9 @@ import tensorflow as tf
 EpsDelta = collections.namedtuple("EpsDelta", ["spent_eps", "spent_delta"])
 
 class AmortizedAccountant(object):
-  """Keep track of privacy spending in an amortized way.
-  AmortizedAccountant accumulates the privacy spending by assuming
-  all the examples are processed uniformly at random so the spending is
-  amortized among all the examples. And we assume that we use Gaussian noise
-  so the accumulation is on eps^2 and delta, using advanced composition.
-  """
 
   def __init__(self, total_examples):
-    """Initialization. Currently only support amortized tracking.
-    Args:
-      total_examples: total number of examples.
-    """
+   
 
     assert total_examples > 0
     self._total_examples = total_examples
@@ -34,18 +25,7 @@ class AmortizedAccountant(object):
 
   def accumulate_privacy_spending(self, eps_delta, unused_sigma,
                                   num_examples):
-    """Accumulate the privacy spending.
-    Currently only support approximate privacy. Here we assume we use Gaussian
-    noise on randomly sampled batch so we get better composition: 1. the per
-    batch privacy is computed using privacy amplication via sampling bound;
-    2. the composition is done using the composition with Gaussian noise.
-    Args:
-      eps_delta: EpsDelta pair which can be tensors.
-      unused_sigma: the noise sigma. Unused for this accountant.
-      num_examples: the number of examples involved.
-    Returns:
-      a TensorFlow operation for updating the privacy spending.
-    """
+  
 
     eps, delta = eps_delta
     with tf.control_dependencies(
@@ -61,61 +41,13 @@ class AmortizedAccountant(object):
                         tf.compat.v1.assign_add(self._delta_sum, amortize_delta)])
 
   def get_privacy_spent(self, target_eps=None):
-    """Report the spending so far.
-    Args:
-      sess: the session to run the tensor.
-      target_eps: the target epsilon. Unused.
-    Returns:
-      the list containing a single EpsDelta, with values as Python floats (as
-      opposed to numpy.float64). This is to be consistent with
-      MomentAccountant which can return a list of (eps, delta) pair.
-    """
+  
     unused_target_eps = target_eps
     eps_squared_sum, delta_sum = ([self._eps_squared_sum, self._delta_sum])
     return [EpsDelta(math.sqrt(eps_squared_sum), float(delta_sum))]
 
 class MomentsAccountant(object):
-  """Privacy accountant which keeps track of moments of privacy loss.
-  Note: The constructor of this class creates tf.Variables that must
-  be initialized with tf.global_variables_initializer() or similar calls.
-  MomentsAccountant accumulates the high moments of the privacy loss. It
-  requires a method for computing differenital moments of the noise (See
-  below for the definition). So every specific accountant should subclass
-  this class by implementing _differential_moments method.
-  Denote by X_i the random variable of privacy loss at the i-th step.
-  Consider two databases D, D' which differ by one item. X_i takes value
-  log Pr[M(D')==x]/Pr[M(D)==x] with probability Pr[M(D)==x].
-  In MomentsAccountant, we keep track of y_i(L) = log E[exp(L X_i)] for some
-  large enough L. To compute the final privacy spending,  we apply Chernoff
-  bound (assuming the random noise added at each step is independent) to
-  bound the total privacy loss Z = sum X_i as follows:
-    Pr[Z > e] = Pr[exp(L Z) > exp(L e)]
-              < E[exp(L Z)] / exp(L e)
-              = Prod_i E[exp(L X_i)] / exp(L e)
-              = exp(sum_i log E[exp(L X_i)]) / exp(L e)
-              = exp(sum_i y_i(L) - L e)
-  Hence the mechanism is (e, d)-differentially private for
-    d =  exp(sum_i y_i(L) - L e).
-  We require d < 1, i.e. e > sum_i y_i(L) / L. We maintain y_i(L) for several
-  L to compute the best d for any give e (normally should be the lowest L
-  such that 2 * sum_i y_i(L) / L < e.
-  We further assume that at each step, the mechanism operates on a random
-  sample with sampling probability q = batch_size / total_examples. Then
-    E[exp(L X)] = E[(Pr[M(D)==x / Pr[M(D')==x])^L]
-  By distinguishign two cases of wether D < D' or D' < D, we have
-  that
-    E[exp(L X)] <= max (I1, I2)
-  where
-    I1 = (1-q) E ((1-q) + q P(X+1) / P(X))^L + q E ((1-q) + q P(X) / P(X-1))^L
-    I2 = E (P(X) / ((1-q) + q P(X+1)))^L
-  In order to compute I1 and I2, one can consider to
-    1. use an asymptotic bound, which recovers the advance composition theorem;
-    2. use the closed formula (like GaussianMomentsAccountant);
-    3. use numerical integration or random sample estimation.
-  Dependent on the distribution, we can often obtain a tigher estimation on
-  the moments and hence a more accurate estimation of the privacy loss than
-  obtained using generic composition theorems.
-  """
+
 
   __metaclass__ = abc.ABCMeta
 
@@ -139,43 +71,17 @@ class MomentsAccountant(object):
                          for moment_order in self._moment_orders]
 
   @abc.abstractmethod
-  def _compute_log_moment(self, sigma, q, moment_order):
-    """Compute high moment of privacy loss.
-    Args:
-      sigma: the noise sigma, in the multiples of the sensitivity.
-      q: the sampling ratio.
-      moment_order: the order of moment.
-    Returns:
-      log E[exp(moment_order * X)]
-    """
+  
     pass
 
   def accumulate_privacy_spending(self, unused_eps_delta,
                                   sigma, num_examples):
-    """Accumulate privacy spending.
-    In particular, accounts for privacy spending when we assume there
-    are num_examples, and we are releasing the vector
-    (sum_{i=1}^{num_examples} x_i) + Normal(0, stddev=l2norm_bound*sigma)
-    where l2norm_bound is the maximum l2_norm of each example x_i, and
-    the num_examples have been randomly selected out of a pool of
-    self.total_examples.
-    Args:
-      unused_eps_delta: EpsDelta pair which can be tensors. Unused
-        in this accountant.
-      sigma: the noise sigma, in the multiples of the sensitivity (that is,
-        if the l2norm sensitivity is k, then the caller must have added
-        Gaussian noise with stddev=k*sigma to the result of the query).
-      num_examples: the number of examples involved.
-    Returns:
-      a TensorFlow operation for updating the privacy spending.
-    """
+   
     q = tf.cast(num_examples, tf.float64) * 1.0 / self._total_examples
 
     moments_accum_ops = []
     for i in range(len(self._log_moments)):
       moment = self._compute_log_moment(sigma, q, self._moment_orders[i])
-      moments_accum_ops.append(tf.compat.v1.assign_add(self._log_moments[i], moment))
-      #moments_accum_ops.append(tf.Variable.assign_add(self._log_moments[i], moment))
     return tf.group(*moments_accum_ops)
 
   def _compute_delta(self, log_moments, eps, lr):
@@ -209,16 +115,6 @@ class MomentsAccountant(object):
     return min_eps
 
   def get_privacy_spent(self, target_eps=None, target_deltas=None):
-    """Compute privacy spending in (e, d)-DP form for a single or list of eps.
-    Args:
-      target_eps: a list of target epsilon's for which we would like to
-        compute corresponding delta value.
-      target_deltas: a list of target deltas for which we would like to
-        compute the corresponding eps value. Caller must specify
-        either target_eps or target_delta.
-    Returns:
-      A list of EpsDelta pairs.
-    """
     assert (target_eps is None) ^ (target_deltas is None)
     eps_deltas = []
     log_moments = self._log_moments
@@ -226,7 +122,7 @@ class MomentsAccountant(object):
     if target_eps is not None:
       for eps in target_eps:
         eps_deltas.append(
-            EpsDelta(eps, self._compute_delta(log_moments_with_order, eps, 0.001)))
+            EpsDelta(eps, self._compute_delta(log_moments_with_order)))
     else:
       assert target_deltas
       for delta in target_deltas:
@@ -236,33 +132,6 @@ class MomentsAccountant(object):
 
 
 class GaussianMomentsAccountant(MomentsAccountant):
-  """MomentsAccountant which assumes Gaussian noise.
-  GaussianMomentsAccountant assumes the noise added is centered Gaussian
-  noise N(0, sigma^2 I). In this case, we can compute the differential moments
-  accurately using a formula.
-  For asymptotic bound, for Gaussian noise with variance sigma^2, we can show
-  for L < sigma^2,  q L < sigma,
-    log E[exp(L X)] = O(q^2 L^2 / sigma^2).
-  Using this we derive that for training T epoches, with batch ratio q,
-  the Gaussian mechanism with variance sigma^2 (with q < 1/sigma) is (e, d)
-  private for d = exp(T/q q^2 L^2 / sigma^2 - L e). Setting L = sigma^2,
-  Tq = e/2, the mechanism is (e, exp(-e sigma^2/2))-DP. Equivalently, the
-  mechanism is (e, d)-DP if sigma = sqrt{2 log(1/d)}/e, q < 1/sigma,
-  and T < e/(2q). This bound is better than the bound obtained using general
-  composition theorems, by an Omega(sqrt{log k}) factor on epsilon, if we run
-  k steps. Since we use direct estimate, the obtained privacy bound has tight
-  constant.
-  For GaussianMomentAccountant, it suffices to compute I1, as I1 >= I2,
-  which reduce to computing E(P(x+s)/P(x+s-1) - 1)^i for s = 0 and 1. In the
-  companion gaussian_moments.py file, we supply procedure for computing both
-  I1 and I2 (the computation of I2 is through multi-precision integration
-  package). It can be verified that indeed I1 >= I2 for wide range of parameters
-  we have tried, though at the moment we are unable to prove this claim.
-  We recommend that when using this accountant, users independently verify
-  using gaussian_moments.py that for their parameters, I1 is indeed larger
-  than I2. This can be done by following the instructions in
-  gaussian_moments.py.
-  """
 
   def __init__(self, total_examples, moment_orders=28):
     """Initialization.
@@ -274,18 +143,6 @@ class GaussianMomentsAccountant(MomentsAccountant):
     self._binomial_table = GenerateBinomialTable(self._max_moment_order)
 
   def _differential_moments(self, sigma, s, t):
-    """Compute 0 to t-th differential moments for Gaussian variable.
-        E[(P(x+s)/P(x+s-1)-1)^t]
-      = sum_{i=0}^t (t choose i) (-1)^{t-i} E[(P(x+s)/P(x+s-1))^i]
-      = sum_{i=0}^t (t choose i) (-1)^{t-i} E[exp(-i*(2*x+2*s-1)/(2*sigma^2))]
-      = sum_{i=0}^t (t choose i) (-1)^{t-i} exp(i(i+1-2*s)/(2 sigma^2))
-    Args:
-      sigma: the noise sigma, in the multiples of the sensitivity.
-      s: the shift.
-      t: 0 to t-th moment.
-    Returns:
-      0 to t-th moment as a tensor of shape [t+1].
-    """
     assert t <= self._max_moment_order, ("The order of %d is out "
                                          "of the upper bound %d."
                                          % (t, self._max_moment_order))
@@ -301,14 +158,7 @@ class GaussianMomentsAccountant(MomentsAccountant):
     return z
 
   def _compute_log_moment(self, sigma, q, moment_order):
-    """Compute high moment of privacy loss.
-    Args:
-      sigma: the noise sigma, in the multiples of the sensitivity.
-      q: the sampling ratio.
-      moment_order: the order of moment.
-    Returns:
-      log E[exp(moment_order * X)]
-    """
+ 
     assert moment_order <= self._max_moment_order, ("The order of %d is out "
                                                     "of the upper bound %d."
                                                     % (moment_order,
@@ -326,18 +176,11 @@ class GaussianMomentsAccountant(MomentsAccountant):
                                      tf.float64)))
 
 def GenerateBinomialTable(m):
-  """Generate binomial table.
-  Args:
-    m: the size of the table.
-  Returns:
-    A two dimensional array T where T[i][j] = (i choose j),
-    for 0<= i, j <=m.
-  """
+
   table = numpy.zeros((m + 1, m + 1), dtype=numpy.float64)
   for i in range(m + 1):
     table[i, 0] = 1
   for i in range(1, m + 1):
-    for j in range(1, m + 1):
       v = table[i - 1, j] + table[i - 1, j -1]
       assert not math.isnan(v) and not math.isinf(v)
       table[i, j] = v
